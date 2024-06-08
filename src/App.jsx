f8888966f1132f3e9a68ee926df5295c4fbf7f64
Ataur Rehman <rehmangour04@gmail.com>
2024-06-08 20:03:56 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <Outlet />
+      <DownloadApp />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
